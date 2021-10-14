1e6d2ac090e92ea786ef923837f24cbeb3cc9d96
rahul tyagi <tyagi2141@gmail.com>
2021-10-14 12:48:10 +0530

    Background color for item

--- app/src/main/java/com/example/shaadi/ui/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/shaadi/ui/adapter/HomeAdapter.kt
@@ -48,16 +48,20 @@ class HomeAdapter(var eventsList: List<Results>, private val onTabCallback: OnTa
                     context.getDrawable(R.drawable.ic_disable_cancel)
                 itemBinding.btnAcceptId.background =
                     context.getDrawable(R.drawable.ic_baseline_check_circle_24)
+                itemBinding.containeeId.setBackgroundColor(context.resources.getColor(R.color.red))
+
             } else if (result.statusFlag == CommonConstant.APPROVED) {
                 itemBinding.btnDeclineId.background =
                     context.getDrawable(R.drawable.ic_baseline_cancel_24)
                 itemBinding.btnAcceptId.background =
                     context.getDrawable(R.drawable.ic_disable_check_circle)
+                itemBinding.containeeId.setBackgroundColor(context.resources.getColor(R.color.green))
+
             } else {
                 itemBinding.btnDeclineId.background =
                     context.getDrawable(R.drawable.ic_baseline_cancel_24)
-                itemBinding.btnAcceptId.background =
-                    context.getDrawable(R.drawable.ic_baseline_check_circle_24)
+                itemBinding.btnAcceptId.background = context.getDrawable(R.drawable.ic_baseline_check_circle_24)
+                itemBinding.containeeId.setBackgroundColor(context.resources.getColor(R.color.white))
             }
 
 
